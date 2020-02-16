a882519cbb19cbcbce9c77b1c4d87d632cfcf4dd
Mostafa <mostafa_gho@yahoo.com>
2020-02-16 10:42:54 +0330

    first

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
-const { Parser } = require('json2csv');
 const fs = require("fs");
 const { exec } = require('child_process');
 const express = require('express');
 const app = express();
+settings = require('./settings.js');
 
-const myfunc = require('./depend.js');
+myfunc = require('./depend.js');
 
-const save_dir = "/home/mostafa/WFM/results/";
-const ping_dest = '8.8.8.8';
-const trace_count = 4;
-
-exec('mkdir ' + save_dir);
-
+exec('mkdir ' + settings.save_dir);
+myfunc.trace_update();
 //save in 
 // myfunc.init_ping(ping_dest, trace_count);
 
-var stdin = process.openStdin();
-stdin.addListener("data", function(d) {
-    if (d.toString().trim() == "save"){
-        for(var k in ping_res) {
-            const json2csvParser = new Parser();
-            csver = json2csvParser.parse(ping_res[k]);
-            fs.writeFileSync(save_dir + k.toString() + ".csv", csver);
-        }
-        console.log("done!");
-    }
-  });
-
 app.get('/list',(req, res) => {
-    fs.readdir(save_dir, function(err, items) {   
+    fs.readdir(settings.save_dir, function(err, items) {   
         html_ret = "";   
         for (var i=0; i<items.length; i++) {
             if(items[i].substr(items[i].length - 4) == ".txt" || items[i].substr(items[i].length - 4) == ".csv"){
@@ -41,15 +25,16 @@ app.get('/list',(req, res) => {
 
 app.get('/download',(req, res) => {
     let url = req.query.dl;
-    res.download(save_dir + "/" + url); // Set disposition and send it.
+    res.download(settings.save_dir + "/" + url); // Set disposition and send it.
 });
 
 app.get('/uptrace', (req, res) =>{
-    myfunc.uptrace();
+    myfunc.trace_update();
+    res.send("trace updated!")
 });
 
 app.get('/start',(req, res) => { 
-    myfunc.trace_plus_ping(ping_dest, trace_count);
+    myfunc.trace_plus_ping();
     console.log("test started");
     
     res.send("test initiated!");
--- depend.js
+++ depend.js
@@ -1,74 +1,46 @@
-const ping = require('net-ping');
 const { exec } = require('child_process');
 const { Parser } = require('json2csv');
 const fs = require("fs");
-
-const save_dir = "/home/mostafa/WFM/results/";
+traceroute = require('traceroute');
+var ping = require('ping');
+settings = require('./settings.js');
 
 let ping_res = {};
 let trace_res = [];
+
 let ping_timer;
-let ping_intervals = 0.5;
-let ping_clock_before = 5;
-let iperf_clock = 1;
-let ping_clock_after = 5;
-let iperf_server = "";
-let iperf_textfile = "";
-
 let test_counter = 0;
+let path_maker = "";
 
-var p = ping.createSession({
-    networkProtocol: ping.NetworkProtocol.IPv4,
-    packetSize: 16,
-    retries: 1,
-    sessionId: (process.pid % 65535),
-    timeout: 2000,
-    ttl: 128
-});
+// let pinger = (ip) => {
+//     return new Promise((resolve, reject) => {
+//         p.pingHost(ip, (error, target, sent, rcvd) => {
+//             if(!error){
+//                 resolve(rcvd - sent);
+//             } else {
+//                 reject(new Error(error))
+//             }
+//         })
+//     })   
+// }
 let pinger = (ip) => {
     return new Promise((resolve, reject) => {
-        p.pingHost(ip, (error, target, sent, rcvd) => {
-            if(!error){
-                resolve(rcvd - sent);
-            } else {
-                reject(new Error(error))
-            }
-        })
+        ping.promise.probe(ip)
+                    .then((res) => {
+                        resolve(res.time);
+                    })
+                .catch(e => {
+                    console.log(e)
+                })
     })   
 }
-function feedCb (error, target, ttl, sent, rcvd) {
-    var ms = rcvd - sent;
-    if (error) {
-        if (error instanceof ping.TimeExceededError) {
-            trace_res.push(error.source);
-        } else {
-        }
-    } else {
-    }
-}
-function doneCb (error, target) {
-    if (error){
-        console.log (target + ": " + error.toString ());
-    }
-
-    ping_traces();
-}
-function doneCb3 (error, target) {
-    if (error){
-        console.log (target + ": " + error.toString ());
-    }
-    console.log("trace list updated!");
-    
-}
-function doneCb2 (error, target) {
-    if (error){
-        console.log (target + ": " + error.toString ());
-    }
+function ping_traces1(){    
     trace_res.forEach(trace_ip => {
-        setTimeout(() =>{
-            ping_timer = setInterval(() => {
-                pinger(trace_ip)
-                    .then(ms => {
+        ping_timer = setInterval(() => {
+                ping.promise.probe(trace_ip)
+                    .then((res) => {
+                        console.log(ping_res);
+                        
                         if (ping_res[trace_ip] == undefined){
                             ping_res[trace_ip] = []
                         }
@@ -84,18 +56,35 @@ function doneCb2 (error, target) {
                         ping_res[trace_ip].push({
                             "dest":trace_ip,
                             "date":date_ob,
-                            "rtt":ms
-                        })        
+                            "rtt":res.time
+                        }) 
                     })
                     .catch(e => {
                         console.log(e)
                     })
-            }, 1000 * ping_intervals)
-        }, ping_clock_before * 1000);
-    });
+        }, 1000 * settings.ping_intervals)
+    })
+    
+    setTimeout(() =>{
+        iperf_handler("", Date());    
+        console.log("iperf done");
+                
+    }, settings.ping_clock_before * 1000);
+    setTimeout(() =>{
+        clearTimeout(settings.ping_timer);       
+
+        for(var k in ping_res) {
+            const json2csvParser = new Parser();
+            csver = json2csvParser.parse(ping_res[k]);
+            fs.writeFileSync(path_maker + k.toString() + ".csv", csver);
+        }
+        console.log("test done!");
+        
+    }, (settings.ping_clock_after + settings.ping_clock_before) * 1000);
+    console.log(ping_res);
+    
 }
-function ping_traces(){
-    console.log(trace_res);
+function ping_traces(){    
     trace_res.forEach(trace_ip => {
         ping_timer = setInterval(() => {
             pinger(trace_ip)
@@ -116,20 +105,21 @@ function ping_traces(){
                         "dest":trace_ip,
                         "date":date_ob,
                         "rtt":ms
-                    })                            
+                    })        
                 })
                 .catch(e => {
                     console.log(e)
                 })
-        }, 1000 * ping_intervals)
-    });
+        }, 1000 * settings.ping_intervals)
+    })
+    
     setTimeout(() =>{
         iperf_handler("", Date());    
         console.log("iperf done");
                 
-    }, ping_clock_before * 1000);
+    }, settings.ping_clock_before * 1000);
     setTimeout(() =>{
-        clearTimeout(ping_timer);       
+        clearTimeout(settings.ping_timer);       
 
         for(var k in ping_res) {
             const json2csvParser = new Parser();
@@ -138,33 +128,47 @@ function ping_traces(){
         }
         console.log("test done!");
         
-    }, (ping_clock_after + ping_clock_before) * 1000);
-    
+    }, (settings.ping_clock_after + settings.ping_clock_before) * 1000);
+    console.log(ping_res);
     
 }
-function iperf_handler(iperf_ip, text_file_name) {
-    exec('iperf3 -c ' + iperf_ip + " -J > " + save_dir + text_file_name + ".txt");
-}
-function trace_plus_ping(dest, trace_count){
+function trace_plus_ping(){
     //add date to path
-    path_maker = save_dir + "test" + test_counter.toString() 
-    + "_" + (ping_clock_before + ping_clock_after + iperf_clock).toString() + "sec_" 
+    path_maker = settings.save_dir + "test" + test_counter.toString() 
+    + "_" + (settings.ping_clock_before + settings.ping_clock_after + settings.iperf_clock).toString() + "sec" 
     + "/" ;
     exec("mkdir " + path_maker);
-    test_counter++;
-
-    console.log(path_maker);        
+    test_counter++;       
     if (trace_res.length == 0){
-        p.traceRoute (dest, trace_count, feedCb, doneCb);   
+        console.log("trace list empty, performing traceroute!");
+        trace_update();
     }
     else {
+        console.log(trace_res);
+        
         ping_traces();
     }
 }
-function init_ping(dest, trace_count){
-    p.traceRoute (dest, trace_count, feedCb, doneCb2);   
+function iperf_handler() {
+    console.log('iperf3 -c ' + settings.iperf_server_address + " -J > " + path_maker + settings.iperf_dest_file + ".txt");
+    
+    exec('iperf3 -c ' + settings.iperf_server_address + " -J > " + path_maker + settings.iperf_dest_file + ".txt");
 }
-function uptrace(){
-    p.traceRoute (dest, trace_count, feedCb, doneCb3);
+function init_ping(){
 }
-module.exports = {trace_plus_ping, init_ping}
+function trace_update(){
+    trace_res=[];
+    traceroute.trace(settings.destination, (err,hops) => {
+        if (!err) {
+            for(var k in hops){
+                if(k > settings.trace_depth)
+                    break;
+                if(Object.keys(hops[k]) != false){
+                    trace_res.push(Object.keys(hops[k])[0]);
+                }
+            }
+        }
+          
+    });
+}
+module.exports = {trace_plus_ping, init_ping, trace_update}
