74dd86ff0a507b49f2407f36449770731002ce01
Mostafa <mostafa_gho@yahoo.com>
2020-02-17 12:55:05 +0330

    sending broacasts

---
+++ request.js
@@ -0,0 +1,20 @@
+var PORT = 6024;
+var BROADCAST_ADDR = "192.168.43.255";
+var dgram = require('dgram'); 
+var server = dgram.createSocket("udp4"); 
+
+function send_broadcast(){
+    server.bind(() => {
+        server.setBroadcast(true);
+        setInterval(broadcastNew, 10000);
+    });
+}
+
+function broadcastNew() {
+    var message = new Buffer.alloc("Hello Client!".length, "Hello Client!")
+    server.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
+        console.log("Sent '" + message + "'");
+    });
+}
+
+module.exports = {send_broadcast}
