de9dc31fe5c1229d8d7aaef7e34cff0fc60c1fdd
Mostafa <mostafa_gho@yahoo.com>
2020-02-23 11:50:45 +0330

    improved functions

--- TestModules/depend.js
+++ TestModules/depend.js
@@ -9,8 +9,6 @@ let ping_res = {};
 let trace_res = [];
 
 let ping_timers=[];
-let test_counter = 0;
-let path_maker = "";
 
 
 // fixed and running
@@ -60,6 +58,11 @@ function init_ping(){
     
 }
 
+// clear initial pings
+function clear_ping(){
+    clearTimeout(ping_timers);
+}
+
 // update traceroutes
 function trace_update(){
     trace_res=[];
@@ -77,76 +80,6 @@ function trace_update(){
     });
 }
 
-// deprecated
-function ping_traces(){
-    trace_res.forEach(trace_ip => {
-        ping_timers.push(setInterval(() => {
-            ping.promise.probe(trace_ip)
-                .then((res) => {                    
-                    if (ping_res[trace_ip] == undefined){
-                        ping_res[trace_ip] = []
-                    }
-                    //shamsi
-                    // var shamsi = "";
-                    // let date_ob = new Date(Date.now());
-                    // shamsi = date_ob.toLocaleString('en-US-u-ca-persian', {
-                    //     timeZone: 'Asia/Tehran',
-                    //     hourCycle: 'h24'
-                    // });
-                    //miladi
-                    let date_ob = Date();
-                    ping_res[trace_ip].push({
-                        "dest":trace_ip,
-                        "date":date_ob,
-                        "rtt":res.time
-                    }) 
-                })
-                .catch(e => {
-                    console.log(e)
-                })
-        }, 1000 * settings.ping_intervals))
-    })
-    
-    setTimeout(() =>{
-        iperf_handler("", Date());    
-        console.log("iperf done");
-                
-    }, settings.ping_clock_before * 1000);
-    setTimeout(() =>{
-        ping_timers.forEach(timer => {
-            clearTimeout(timer);
-        });       
-
-        for(var k in ping_res) {
-            const json2csvParser = new Parser();
-            csver = json2csvParser.parse(ping_res[k]);
-            fs.writeFileSync(path_maker + k.toString() + ".csv", csver);
-        }
-        console.log("test done!");
-        
-    }, (settings.ping_clock_after + settings.ping_clock_before) * 1000);
-    
-}
-
-// deprecated
-function trace_plus_ping(){
-    //add date to path
-    path_maker = settings.save_dir + "test" + test_counter.toString() 
-    + "_" + (settings.ping_clock_before + settings.ping_clock_after + settings.iperf_clock).toString() + "sec" 
-    + "/" ;
-    exec("mkdir " + path_maker);
-    test_counter++;       
-    if (trace_res.length == 0){
-        console.log("trace list empty, performing traceroute!");
-        trace_update();
-    }
-    else {
-        console.log("trace list is: " + trace_res);
-        
-        ping_traces();
-    }
-}
-
 // needs more input args
 function iperf_handler() {
     let date_ob = Date();
@@ -156,11 +89,11 @@ function iperf_handler() {
     let iperf_path = settings.save_dir + settings.iperf_dest_file + "/";
     exec("mkdir " + iperf_path);
 
-    console.log(iperf_path);
+    // command to execute!
     let comm = 'iperf3 -c ' + settings.iperf_server_address + " -J > " + iperf_path + date_ob + ".txt";
+    
     console.log(comm);
 
-    // exec('iperf3 -c ' + settings.iperf_server_address + " -J > " + iperf_path + ".txt");
     exec(comm, (err, stdout, stderr) => {
         if (err) {
             console.log(err);
